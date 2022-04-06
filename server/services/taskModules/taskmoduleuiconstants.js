dabf71a5d9263a5faec225694b60bc443d474998
Shuvam Sengupta <shuvamsengupta58824@gmail.com>
2022-04-06 13:25:48 +0530

    task module

--- server/services/taskModules/taskmoduleuiconstants.js
+++ server/services/taskModules/taskmoduleuiconstants.js
@@ -1,6 +1,7 @@
 const { UISettings } = require("./uisettings");
 const { TaskModulesIds } = require("./taskmoduleid");
 
+//ID
 const TaskModuleUIConstants = {};
 
 module.exports.TaskModuleUIConstants = TaskModuleUIConstants;
